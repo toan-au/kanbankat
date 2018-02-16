e1387c3cbdab2a305451f514fea4ad4490d22f30
DeftCorgi <toanau123@gmail.com>
2018-02-16 16:39:50 +1100

    added keys and passport

--- routes/auth.js
+++ routes/auth.js
@@ -1,3 +1,19 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
+// configure passport
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: '/'
+    },
+    (accessToken, refreshToken, profile, cb) => {}
+  )
+);
+
 module.exports = app => {
   app.get('/api/test', (req, res) => {
     res.send({ success: true });
