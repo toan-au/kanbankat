5c4970b4da88d5477c7eeb8a2a69bc3d043f40ea
DeftCorgi <toanau123@gmail.com>
2018-02-19 17:49:36 +1100

    added googleId'

--- routes/auth.js
+++ routes/auth.js
@@ -17,9 +17,13 @@ passport.use(
   )
 );
 
+// auth routes
+
 module.exports = app => {
   app.get(
     '/auth/google',
     passport.authenticate('google', { scope: ['profile'] })
   );
+
+  app.get('/auth/current', (req, res) => res.send(req.user));
 };
