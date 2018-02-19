e4478e7e01cabb5e71bf87ff1aaafc0810dcebbc
DeftCorgi <toanau123@gmail.com>
2018-02-19 17:30:26 +1100

    added auth middleware

--- server.js
+++ server.js
@@ -1,6 +1,20 @@
 const express = require('express');
+const cookieSession = require('cookie-session');
+const keys = require('./config/keys');
+const passport = require('passport');
 
 const app = express();
+
+// middleware
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 // models
 
