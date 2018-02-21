f8aaf794e5adc066fb123eb7e05913297e1e740f
DeftCorgi <toanau123@gmail.com>
2018-02-21 20:16:36 +1100

    initial api creation

--- models/board.js
+++ models/board.js
@@ -1,4 +1,4 @@
-const mongoos = require('mongoose');
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
--- routes/board.js
+++ routes/board.js
@@ -3,5 +3,9 @@ const mongoose = require('mongoose');
 const Board = mongoose.model('boards');
 
 module.exports = app => {
-  app.get('/board/new', (req, res) => {});
+  app.get('/board/new', async (req, res) => {
+    const { name } = req.body;
+    const board = await new Board({ name });
+    res.send(board);
+  });
 };
