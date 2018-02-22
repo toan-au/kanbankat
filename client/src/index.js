02fe78c5a396daf851c116c40beae6c6a3b7a493
DeftCorgi <toanau123@gmail.com>
2018-02-23 10:43:18 +1100

    added bootstrap 4.0.0

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js.map';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
