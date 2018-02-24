5d62e222fb2f0c3b10cbbcdd1b18b3b5dff734d3
DeftCorgi <toanau123@gmail.com>
2018-02-24 15:08:28 +1100

    added axios to window

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js.map';
+
+// include axios in the window to test api
+if (process.env.NODE_ENV === 'development') {
+  window.axios = axios;
+}
 
 ReactDOM.render(
   <Router>
