5f6bc8bb25736691758f015fb167abe9e82da79b
Toan Au <toanau.dev@gmail.com>
2024-02-17 17:11:25 +1100

    Added vite client project

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,30 +5,28 @@ import { getUser } from '../actions/user';
 import { withRouter } from 'react-router-dom';
 import Topbar from './Topbar';
 
-class App extends Component {
+const App = () => {
   componentDidMount() {
     this.props.getUser();
   }
 
-  render() {
-    return (
-      <div className="App">
-        <Topbar />
-        <div>
-          <Main />
-        </div>
+  return (
+    <div className="App">
+      <Topbar />
+      <div>
+        <Main />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 const mapStateToProps = state => {
   return { user: state.user };
 };
 
-export default withRouter(
-  connect(
-    mapStateToProps,
-    { getUser }
-  )(App)
-);
+// export default withRouter(
+//   connect(
+//     mapStateToProps,
+//     { getUser }
+//   )(App)
+// );
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const cookieSession = require('cookie-session');
-const keys = require('./config/keys');
-const passport = require('passport');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+const express = require("express");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 // models
-require('./models/user');
-require('./models/board.js');
+require("./models/user");
+require("./models/board.js");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
@@ -20,7 +20,7 @@ app.use(bodyParser.json());
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
-    keys: [keys.cookieKey]
+    keys: [keys.cookieKey],
   })
 );
 
@@ -28,21 +28,21 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 // routes
-require('./routes/auth')(app);
-require('./routes/board')(app);
+require("./routes/auth")(app);
+require("./routes/board")(app);
 
 // client app
-if (process.env.NODE_ENV === 'production') {
-  const path = require('path');
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
   // express will serve our static bundle files
-  app.use(express.static('client/build/'));
+  app.use(express.static("client/build/"));
 
   // express will serve our client app if it doesn't recognize the route
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client-vite", "dist", "index.html"));
   });
 }
 
 app.listen(process.env.PORT || 5000, () => {
-  console.log('server has been started');
+  console.log("server has been started");
 });
