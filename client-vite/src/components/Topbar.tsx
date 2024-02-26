4a984f51300a50ade0ef38ac117c98170c651408
Toan Au <toanau.dev@gmail.com>
2024-02-26 20:50:12 +1100

    topbar menu reacts based on logged in status

--- client-vite/src/components/Topbar.tsx
+++ client-vite/src/components/Topbar.tsx
@@ -7,9 +7,58 @@ import { Link } from "react-router-dom";
 function Topbar() {
   const user = useSelector((state: RootState) => state.currentUser);
   const dispatch = useDispatch<AppDispatch>();
+
   useEffect(() => {
     dispatch(getUserAsync());
   }, [dispatch]);
+
+  function LoggedOutMenu() {
+    return (
+      <>
+        <li>
+          <a
+            href="#"
+            className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+            aria-current="page"
+          >
+            Features
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+          >
+            About
+          </a>
+        </li>
+        <li>
+          <a
+            href="#"
+            className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+          >
+            Plans
+          </a>
+        </li>
+      </>
+    );
+  }
+
+  function LoggedInMenu() {
+    return (
+      <>
+        <li>
+          <a
+            href="/dashboard"
+            className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
+            aria-current="page"
+          >
+            Dashboard
+          </a>
+        </li>
+      </>
+    );
+  }
 
   return (
     <nav className="bg-white border-gray-200 dark:bg-gray-900">
@@ -77,31 +126,7 @@ function Topbar() {
           id="navbar-cta"
         >
           <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                aria-current="page"
-              >
-                Features
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-              >
-                Plans
-              </a>
-            </li>
+            {user.loggedIn ? <LoggedInMenu /> : <LoggedOutMenu />}
           </ul>
         </div>
       </div>
